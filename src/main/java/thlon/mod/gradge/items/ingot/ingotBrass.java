1c2cc189bc5a90c1f773f1a8a2fdd5d47247b761
YuiChanDev <yuthlon157@gmail.com>
2023-01-11 14:43:15 +0900

    Added File

---
+++ src/main/java/thlon/mod/gradge/items/ingot/ingotBrass.java
@@ -0,0 +1,8 @@
+package thlon.mod.gradge.items.ingot;
+
+public class ingotBrass extends ingotItem{
+    public ingotBrass(){
+        this.setTextureName("gradge:ingot/ingotbrass");
+        this.setUnlocalizedName("ingotBrass");
+    }
+}
